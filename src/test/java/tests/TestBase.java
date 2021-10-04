f353e3390919b75decd5eca2dc878ce2fde4298e
Faustyn <faustynlewandowski@gmail.com>
2021-10-04 13:40:23 +0200

    Add Allure framework

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,27 +14,24 @@ import java.util.Properties;
 
 public class TestBase {
 
-
-    @BeforeClass
     @Step("Loading configuration from configuration.properties")
+    @BeforeClass
     public void beforeClass(){
         PropertiesLoader propertiesLoader = new PropertiesLoader();
         Properties propertiesFromFile = propertiesLoader.getPropertiesFromFile("configuration.properties");
         ConfigurationProperties.setProperties(propertiesFromFile);
     }
 
-
-    @BeforeMethod
     @Step("Setting up browser: {browserType} and open Home Page")
+    @BeforeMethod
     public void beforeTest(){
         DriverManager.getWebDriver();
         DriverUtils.setInitialConfiguration();
         DriverUtils.navigateToPage(TestData.URL);
     }
 
-
-    @AfterMethod
     @Step("Closing browser")
+    @AfterMethod
     public void afterTest() {
         DriverManager.disposeDriver();
     }
